d580f3fe8f22132f838859a160b1ee62a09b1623
jmurrii <jonmurrii@gmail.com>
2020-09-22 13:46:34 +0000

    Created templates folder with relvant routing updates.  Uploaded Images for site

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def index():
 def create():
     return render_template("create.html")
 
+@app.route("/browse")
+def browse():
+    return render_template("browse.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
