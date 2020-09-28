2c9b97acf0139913b2e38fb1acd55a4d86d50367
jmurrii <jonmurrii@gmail.com>
2020-09-28 14:43:03 +0000

    index.html updated with images and info

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
