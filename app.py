294416121b9041aa713ac2575754f8657d7edbcd
jmurrii <jonmurrii@gmail.com>
2020-09-30 09:11:22 +0000

    Added for loop to display data from mongodb

--- app.py
+++ app.py
@@ -1,53 +1,38 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-
-from os import path
-if path.exists("env.py"):
+if os.path.exists("env.py"):
   import env
 
 
 app = Flask(__name__)
+app.secret_key = env.Secret_Key
 app.config["MONGO_DBNAME"] = 'user_uploads'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
-
 mongo = PyMongo(app)
-
 
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
-@app.route("/flaskmongo")
-def flaskmongo():
-    return render_template("flaskmongo.html", books=mongo.db.books.find())
-
-
-
-@app.route("/submit", methods=['GET', 'POST'])
+@app.route("/submit")
 def submit():
-    if request.method == 'POST':
-        book_name = request.form["book_name"]
-        author_name = request.form["author_name"]
-        genre = request.form["genre"]
-        summary = request.form["summary"]
-        added_by = request.form["added_by"]
-        print (book_name, author_name, genre, summary, added_by)
     return render_template("submit.html", page_title="Submit")
-   
 
 
+@app.route('/insert_book', methods=['POST'])
+def insert_book():
+    books = mongo.db.books
+    books.insert_one(request.form.to_dict())
+    return redirect(url_for('browse'))
 
- #@app.route('/accept_data', methods=['post'])
-#def accept_data():
- # path = request.form["{{ url_for('submit.html') }}"]
 
 @app.route("/browse")
 def browse():
-    return render_template("browse.html", page_title="Browse")
+    return render_template("browse.html", page_title="Browse",
+    books=mongo.db.books.find())
 
 
 if __name__ == '__main__':
