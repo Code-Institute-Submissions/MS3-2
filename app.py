9f80def00010d5bb6769ae19d6e6627416a598fd
jmurrii <jonmurrii@gmail.com>
2020-09-30 16:58:55 +0000

    Added wireframes to img folder

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
   import env
 
